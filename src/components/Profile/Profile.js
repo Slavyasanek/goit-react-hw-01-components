94df6c07d54b55606c3b1c3cee559062d42ceddb
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-04-11 15:00:40 +0300

    add proptypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
 const Profile = ({user}) => {
@@ -31,6 +32,18 @@ const Profile = ({user}) => {
             </ul>
         </div>
     );
+}
+
+Profile.prototype = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    })
 }
 
 export default Profile;
